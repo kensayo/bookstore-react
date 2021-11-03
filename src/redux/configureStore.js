17c1ae38357d04d710561898b552ccba29a46003
Kenny Ortega <kennyaoc@gmail.com>
2021-11-02 22:21:56 -0400

    setup ConfigureStore

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
+
+const reducers = combineReducers({
+  booksReducer,
+});
+
+const store = createStore(reducers, applyMiddleware(logger));
+
+export default store;
