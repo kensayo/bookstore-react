38f86d727337b12bc6b9af6996d95ddb6f717a59
Kenny Ortega <kennyaoc@gmail.com>
2021-11-04 15:56:56 -0400

    Add Remove Books

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,30 +1,38 @@
 import React from 'react';
+// import { useSelector } from 'react-redux';
+import RemoveBook from './removeBook';
 
-const BookList = () => (
-  <div className="d-flex flex-row justify-content-center">
-    <div className="d-flex flex-column px-3">
-      <h3>Lorem</h3>
-      <h1>Lorem Ipsun</h1>
-      <span>Subtittle</span>
-      <div>
-        <ul>
-          <li>Comments</li>
-          <li>Remove</li>
-          <li>Edit</li>
-        </ul>
+const BookList = () => {
+  // const bookStore = useSelector((state) => state.book);
+  const bookStore = [{ id: 123, title: 'Lorem', author: 'Ipsum' }];
+  return (
+    <div className="d-flex flex-row justify-content-center my-2">
+      {bookStore.map((book) => (
+        <div className="d-flex flex-column px-3" key={book.id}>
+          <h3>Lorem</h3>
+          <h1>{book.title}</h1>
+          <span>{book.author}</span>
+          <div>
+            <ul>
+              <li>Comments</li>
+              <RemoveBook id={book.id} />
+              <li>Edit</li>
+            </ul>
+          </div>
+        </div>
+      ))}
+      <div className="px-3">
+        <div>Animated Circle</div>
+        <div>XX%</div>
+        <h4>Completed</h4>
+      </div>
+      <div className="px-3">
+        <h3>CURRENT CHAPTER</h3>
+        <p>Chapter 17</p>
+        <button type="button" className="btn btn-primary">Update Progress</button>
       </div>
     </div>
-    <div className="px-3">
-      <div>Animated Circle</div>
-      <div>XX%</div>
-      <h4>Completed</h4>
-    </div>
-    <div className="px-3">
-      <h3>CURRENT CHAPTER</h3>
-      <p>Chapter 17</p>
-      <button type="button" className="btn btn-primary">Update Progress</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default BookList;
